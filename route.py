6098e8b4987969de3955482b159ea9eca999f5af
kouadioamani <kouadio3.amani@uvci.edu.ci>
2026-05-09 00:24:45 +0000

    phase 1 terminé

---
+++ route.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
+routes =Blueprint('routes', __name__)
+@routes.route('/')
+def home():
+    return render_template("home.html")
+@routes.route('/sign_up')
+def sign_up():
+    return render_template("signup.html")
+@routes.route('/login')
+def login():
+    return render_template("login.html")
---
+++ web/__init__.py
@@ -0,0 +1,22 @@
+# from  flask import Flask
+# def create_app():
+#     app = Flask(__name__)
+#     from route import routes
+#     app.register_blueprint(routes, url_prefix='/')
+#     return app
+import os
+from flask import Flask
+
+def create_app():
+    base_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(base_dir, "templates"),
+        static_folder=os.path.join(base_dir, "static")
+    )
+
+    from route import routes
+    app.register_blueprint(routes)
+
+    return app
